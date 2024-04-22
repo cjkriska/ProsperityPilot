497215a8095a8d68d0e19b78f06c3e7a888e6441
cjkriska <charleskriska@gmail.com>
2024-04-22 16:56:11 -0500

    minor style changes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
     <div className="flex">
       <Sidebar />
       <main className="flex grow flex-col p-2.5">
-        <div className="">Debt Free By</div>
+        <div className="text-bold text-lg">Debt Free By</div>
 
         <div className="flex grow flex-col gap-5 p-2.5">
-          <div className="flex h-1/2 flex-col rounded bg-slate-200 p-3 shadow-lg">
+          <div className="text-bold flex h-1/2 flex-col rounded bg-slate-200 p-3 text-lg shadow-lg">
             Summary
           </div>
 
           <div className="flex grow gap-5">
-            <div className="w-1/2 rounded bg-slate-200 p-3 shadow-lg">
+            <div className="text-bold w-1/2 rounded bg-slate-200 p-3 text-lg shadow-lg">
               Debts
             </div>
-            <div className="grow rounded bg-slate-200 p-3 shadow-lg">
+            <div className="text-bold grow rounded bg-slate-200 p-3 text-lg shadow-lg">
               Payoff Plan
             </div>
           </div>
